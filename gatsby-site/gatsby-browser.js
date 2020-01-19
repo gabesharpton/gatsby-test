8cab50edbbf12f34bcce0f3aae229d02e5b74765
gabesharpton <gabriel.sharpton@gmail.com>
2020-01-18 18:54:20 -0600

    set up bootstrap

--- gatsby-site/gatsby-browser.js
+++ gatsby-site/gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+import 'bootstrap/dist/js/bootstrap.min.js'
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/css/bootstrap.min.css";
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -11,6 +12,8 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <button className="btn btn-info btn-lg">Hello there</button>
+    <button className="btn btn-danger btn-block btn-secondary">Hello there</button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
